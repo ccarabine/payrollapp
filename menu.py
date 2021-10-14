5820dbefc3559da2562e1eef867cca08fb11a94d
ccarabine <ccarabine12@gmail.com>
2021-10-14 19:51:10 +0000

    Add for loop to menus

--- menu.py
+++ menu.py
@@ -5,88 +5,137 @@ This is the menu file for a command line interface payroll system for
 displaying each of the menus.
 """
 
+menu_welcome = ["----------------------------------------------"
+                "---------------",
+                "---------------- People Payroll Application ------------"
+                "-----",
+                "------------------------- Welcome ----------------------"
+                "-----",
+                "--------------------------------------------------------"
+                "-----\n"]
+
+
+menu_main_menu = ["------------------------------------------------------"
+                  "-------",
+                  "---------------- People Payroll Application ------------"
+                  "-----",
+                  "------------------------ Main Menu ---------------------"
+                  "-----",
+                  "--------------------------------------------------------"
+                  "-----\n",
+                  "1 Display payroll",
+                  "2 Process / Amend payroll",
+                  "3 Add / Amend employee details",
+                  "4 Close application\n"]
+
+menu_display_payroll = ["------------------------------------------------"
+                        "-------------",
+                        "---------------- People Payroll Application -------"
+                        "----------",
+                        "---------------------- Display Payroll ------------"
+                        "----------",
+                        "---------------------------------------------------"
+                        "----------\n",
+                        "1 Overall employees' pay for week selected",
+                        "2 Employee pay for week selected",
+                        "3 Employers summary for week",
+                        "4 Main menu\n"]
+
+menu_process_amend_payroll = ["---------------------------------------------"
+                              "----------------",
+                              "---------------- People Payroll Application ---"
+                              "--------------",
+                              "----------------- Process / Amend Payroll -----"
+                              "--------------",
+                              "-----------------------------------------------"
+                              "--------------\n",
+                              "1 Add employees' hours",
+                              "2 Amend employee's hours",
+                              "3 Main menu\n"]
+
+
+menu_add_amend_employee = ["--------------------------------------------------"
+                           "-----------",
+                           "---------------- People Payroll Application ------"
+                           "-----------",
+                           "-------------- Add / Amend Employee's Details ----"
+                           "-----------",
+                           "--------------------------------------------------"
+                           "-----------\n",
+                           "\n Feature in next update, returning to main menu "
+                           "\n"]
+
+menu_employees_pay = ["-------------------------------------------------------"
+                      "------",
+                      "---------------- People Payroll Application -----------"
+                      "------",
+                      "---------------------- Employees' pay -----------------"
+                      "------",
+                      "-------------------------------------------------------"
+                      "------\n"]
+
+
+menu_employers_payment_summary = ["-------------------------------------------"
+                                  "------------------",
+                                  "---------------- People Payroll Application"
+                                  "------------------",
+                                  "----------------- Employers payment summary"
+                                  "------------------",
+                                  "-------------------------------------------"
+                                  "------------------\n"]
+
 
 def welcome_menu():
     """
     Displays the welcome screen on load
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("------------------------- Welcome ---------------------------")
-    print("-------------------------------------------------------------\n")
+    for header in menu_welcome:
+        print(header)
 
 
 def main_menu():
     """
     Displays the Main menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("------------------------ Main Menu --------------------------")
-    print("-------------------------------------------------------------\n")
-    print("1 Display payroll")
-    print("2 Process / Amend payroll")
-    print("3 Add / Amend employee details")
-    print("4 Close application\n")
+    for header in menu_main_menu:
+        print(header)
 
 
 def display_payroll_menu():
     """
     Displays the Display payroll menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("---------------------- Display Payroll ----------------------")
-    print("-------------------------------------------------------------\n")
-    print("1 Overall employees' pay for week selected")
-    print("2 Employee pay for week selected")
-    print("3 Employers summary for week")
-    print("4 Main menu\n")
+    for header in menu_display_payroll:
+        print(header)
 
 
 def process_amend_payroll_menu():
     """
     Displays the Process / Amend menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("----------------- Process / Amend Payroll -------------------")
-    print("-------------------------------------------------------------\n")
-    print("")
-    print("1 Add employees' hours")
-    print("2 Amend employee's hours")
-    print("3 Main menu\n")
+    for header in menu_process_amend_payroll:
+        print(header)
 
 
 def add_amend_employee_menu():
     """
     Displays the Add / Amend Employee's Details menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("-------------- Add / Amend Employee's Details ---------------")
-    print("-------------------------------------------------------------\n")
-    print("\n Feature in next update, returning to main menu \n")
+    for header in menu_add_amend_employee:
+        print(header)
 
 
 def employees_pay_menu():
     """
     Displays the Employees' Details menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("---------------------- Employees' pay -----------------------")
-    print(
-        "---------------------------------------------------------"
-        "----\n"
-        )
+    for header in menu_employees_pay:
+        print(header)
 
 
 def employers_payment_summary_menu():
     """
     Displays the Employees' Details menu
     """
-    print("-------------------------------------------------------------")
-    print("---------------- People Payroll Application -----------------")
-    print("----------------- Employers payment summary -----------------")
-    print("-------------------------------------------------------------\n")
+    for header in menu_employers_payment_summary:
+        print(header)
